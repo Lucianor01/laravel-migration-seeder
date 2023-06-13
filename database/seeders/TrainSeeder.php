6bd301e85b021b4941ae93ae588ca89cb0244287
Lucianor01 <lucianorosario02@gmail.com>
2023-06-13 14:57:56 +0200

    Faker Seed

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Train;
+use Faker\Generator as Faker;
+
 class TrainSeeder extends Seeder
 {
     /**
@@ -12,8 +15,22 @@ class TrainSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 100; $i++) {
+            $train = new Train();
+
+            $train->company = $faker->company();
+            $train->departure_station = $faker->randomElement(['Roma', 'Milano', 'Napoli']);
+            $train->arrival_station = $faker->randomElement(['Palermo', 'Rimini', 'Genova']);
+            $train->departure_time = $faker->dateTimeBetween('-1 day', '+1 day');
+            $train->arrival_time = $faker->dateTimeBetween('-1 day', '+2 day');
+            $train->train_code = 'AB' . $faker->randomDigit();
+            $train->wagons_number = $faker->randomDigit();
+            $train->on_time = $faker->boolean(70);
+            $train->cancelled = $faker->boolean(20);
+            $train->save();
+        }
     }
 }
